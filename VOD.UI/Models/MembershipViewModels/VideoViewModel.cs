f9f338257558b4e1e90047f3d427fba4dc3d6331
arteom <arteom.kor@gmail.com>
2022-06-14 20:27:22 +0300

    DTOs added

---
+++ VOD.UI/Models/MembershipViewModels/VideoViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+using VOD.Common.DTOModels.UI;
+
+namespace VOD.UI.Models.MembershipViewModels
+{
+    public class VideoViewModel
+    {
+        public VideoDTO Video { get; set; }
+        public InstructorDTO Instructor { get; set; }
+        public CourseDTO Course { get; set; }
+        public LessonInfoDTO LessonInfo { get; set; }
+    }
+}
