364df5a0a44307d85ef608a8b95f9765d7d77fb5
arteom <arteom.kor@gmail.com>
2022-06-29 18:30:22 +0300

    C+U tests added;

--- VOD.DAL.Tests/ContextTests/CourseTests.cs
+++ VOD.DAL.Tests/ContextTests/CourseTests.cs
@@ -8,6 +8,10 @@ using VOD.Database.Migrations.DbInitializer;
 
 namespace VOD.DAL.Tests.ContextTests
 {
+    /// <summary>
+    /// Integration tests against a dockerized db. See justification at https://docs.microsoft.com/en-us/ef/core/testing/choosing-a-testing-strategy
+    /// </summary>
+
     [TestFixture]
     public class CourseTests
     {
@@ -100,6 +104,62 @@ namespace VOD.DAL.Tests.ContextTests
             Assert.AreEqual("Docker in Nutshell", courses[0].Title);
             Assert.AreEqual("Essential EF Core", courses[1].Title);
 
+        }
+
+        [Test]
+        public void ShoudUpdateCourse()
+        {
+            //Arrange
+            var course = new Course
+            {
+                Title = "Essential EF Core",
+                Description = "Dive into the hidden depths of the Framework",
+                Instructor = new Instructor { Name = "Troelsen Japikse" }
+            };
+
+           //Act
+            _db.Courses.Add(course);
+            _db.SaveChanges();
+            course.Title = "Programming abstractions in C++";
+            _db.Courses.Update(course);
+
+            //Assert
+            Assert.That(_db.Entry(course).State, Is.EqualTo(EntityState.Modified));
+
+            //Act 
+            _db.SaveChanges();
+
+            //Assert
+            Assert.That(_db.Entry(course).State, Is.EqualTo(EntityState.Unchanged));
+            
+            //Arrange
+            VODContext context;
+            
+            //Assert 
+            using (context = new VODContextFactory().CreateDbContext(null)) 
+            {
+                Assert.That(context.Courses.First().Title, Is.EqualTo("Programming abstractions in C++"));
+            }
+        }
+
+        ///Invoking update on the new Entity should throw exception
+        [Test]
+        public void ShouldNotUpdateNonAttachedCourse()
+        {
+            //Arrange
+            var course = new Course
+            {
+                Title = "Essential EF Core",
+                Description = "Dive into the hidden depths of the Framework",
+                Instructor = new Instructor { Name = "Adam Freeman" }
+            };
+
+            //Act
+            _db.Courses.Add(course);
+            course.Title = "Microservices in Action with Nancy";
+
+            //Assert 
+            Assert.Throws<InvalidOperationException>(() => _db.Courses.Update(course));
         }
     }
 }
--- readme.md
+++ readme.md
@@ -1,3 +1,6 @@
 Kind of a test project, utilizing Net Core, Entity Framework, 
-NUnit, Razor pages. 
-Credits to Jonas Fagerberg from whose work heavilly borrowed.
+NUnit, Razor pages, docker. Testing was performed against localDB, conternized in Docker.
+
+Credits to Jonas Fagerberg (C# school) and Phillip Japikse from whose works borrowed.
+
+
