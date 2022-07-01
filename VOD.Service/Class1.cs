212ef1e69cb7cf9e3c3c05b6a7407df202debf24
arteom <arteom.kor@gmail.com>
2022-07-01 18:00:40 +0300

    sol_change

---
+++ VOD.Service/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace VOD.Service
+{
+    public class Class1
+    {
+    }
+}
---
+++ Vod.Database/CourseServices/Concrete/ListCourseService.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using VOD.Database.Contexts;
+
+namespace VOD.Database.CourseServices.Concrete
+{
+    public class ListCourseService
+    {
+        private readonly VODContext _context;
+
+
+    }
+}
