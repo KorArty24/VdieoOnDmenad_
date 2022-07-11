c644a1eb5d98b0a2600d8610a38c723402d0ef9a
arteom <arteom.kor@gmail.com>
2022-07-12 01:59:50 +0300

    adminservices templates added

---
+++ VOD.Service/AdminServices/IAddNewInstructorService.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VOD.Service.AdminServices
+{
+    public interface IAddNewInstructorService
+    {
+        //to be implemented
+    }
+}
