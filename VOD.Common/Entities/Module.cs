49223fba1e87005c83a2780e3d816cec718ecdd2
KorArty24 <48129496+KorArty24@users.noreply.github.com>
2022-10-12 13:22:16 +0300

    Revert "unfinished course refactoring to ddd style"
    This reverts commit 0070525e86c02ce5a56d61f04b1ca5c2e0b0586a.

--- VOD.Common/Entities/Module.cs
+++ VOD.Common/Entities/Module.cs
@@ -11,31 +11,23 @@ namespace VOD.Common.Entities
     public class Module: EntityBase
     {
         private Module() { }
-        internal Module( Course course, string title, int courseId)
-        {
-            Course = course;
-            Title = title;
-            CourseId = courseId;
-        }
 
         [Key]
         public int Id { get; set; }
 
         [MaxLength(80), Required]
-        public string Title { get; private set; }
+        public string Title { get; set; }
 
-        //Fully defined relationship
-        public int CourseId { get; private set; }
+        public int CourseId { get; set; }
 
-        public Course Course { get; private set; }
+        public Course Course { get; set; }
 
         //public List<Video> Videos { get; set; }
-        private ICollection<Video> _videos = new List<Video>();
-        public IReadOnlyCollection<Video> Courses => _videos?.ToList();
+        private HashSet<Course> _courses = new HashSet<Course>();
+        public IReadOnlyCollection<Course> Courses => _courses?.ToList();
 
         //public List<Download> Downloads { get; set; }
         private HashSet<Download> _downloads = new HashSet<Download>();
         public IReadOnlyCollection<Download> Downloads => _downloads?.ToList();
-
     }
 }
