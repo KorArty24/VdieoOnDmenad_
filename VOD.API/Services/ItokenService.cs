32bf641519784bf7a4a60f3a82186956f83a8ded
KorArty24 <48129496+KorArty24@users.noreply.github.com>
2023-01-10 01:25:02 +0300

    TokenController

--- VOD.API/Services/ItokenService.cs
+++ VOD.API/Services/ItokenService.cs
@@ -4,9 +4,9 @@ using VOD.Common.DTOModels.Admin;
 
 namespace VOD.API.Services
 {
-    public interface ItokenService
+    public interface ITokenService
     {
-        public Task<TokenDTO> GenerageTokenAsync(LoginUserDTO loginUserDTO);
+        public Task<TokenDTO> GenerateTokenAsync(LoginUserDTO loginUserDTO);
         public Task<TokenDTO> GetTokenAsync(LoginUserDTO loginUserDTO, string userid);
     }
 
